3b6538e213c50eb8afde13aeb142959e0e796000
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-02 16:27:59 +1000

    Added CarModelRepository

---
+++ CarHub/Data/Repositories/CarModelRepository.cs
@@ -0,0 +1,24 @@
+﻿using CarHub.Data.Models;
+using CarHub.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarHub.Data.Repositories
+{
+    public class CarModelRepository:ICarModelRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarModelRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<CarModel> GetAllModelsByMake(int makeId)
+        {
+            return _context.CarModels.Where(m => m.CarMakeId == makeId).ToList();
+        }
+    }
+}
