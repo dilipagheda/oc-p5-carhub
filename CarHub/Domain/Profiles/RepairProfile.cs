69a6031e4fcd1845841f15981625ea00914af5d7
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-19 06:39:39 +1000

    Added automapper mapping for Repair

---
+++ CarHub/Domain/Profiles/RepairProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CarHub.Data.Models;
+using CarHub.Domain.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarHub.Domain.Profiles
+{
+    public class RepairProfile : Profile
+    {
+        public RepairProfile()
+        {
+
+            CreateMap<Repair, InventoryViewModel>()
+                .ForMember(dest => dest.RepairDescription, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.RepairCost, opt => opt.MapFrom(src => src.Cost))
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+
+            CreateMap<InventoryViewModel, Repair>()
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.RepairDescription))
+                .ForMember(dest => dest.Cost, opt => opt.MapFrom(src => src.RepairCost))
+                .ForMember(dest => dest.CarId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Car, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+        }
+    }
+}
