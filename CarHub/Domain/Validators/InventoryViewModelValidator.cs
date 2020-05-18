3aa203b24b1ea8bb43e99424734ce0d7d5bb442c
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-19 06:38:34 +1000

    Added fluent validations

--- CarHub/Domain/Validators/InventoryViewModelValidator.cs
+++ CarHub/Domain/Validators/InventoryViewModelValidator.cs
@@ -1,4 +1,4 @@
-﻿using CarHub.Domain.ViewModels.Admin;
+﻿using CarHub.Domain.ViewModels;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -7,29 +7,29 @@ using System.Threading.Tasks;
 
 namespace CarHub.Domain.Validators
 {
-    public class AddNewCarViewModelValidator : AbstractValidator<AddNewCarViewModel>
+    public class InventoryViewModelValidator : AbstractValidator<InventoryViewModel>
     {
-        public AddNewCarViewModelValidator()
+        public InventoryViewModelValidator()
         {
             RuleFor(x => x.CarMakeId).NotNull();
-            RuleFor(x => x.CarMakeId).GreaterThan(0).When(x => x.NewCarMake == null);
-            RuleFor(x => x.NewCarMake).NotNull().When(x => x.CarMakeId <= 0);
+            RuleFor(x => x.CarMakeId).GreaterThan(0).When(x => x.CarMakeName == null);
+            RuleFor(x => x.CarMakeName).NotNull().When(x => x.CarMakeId <= 0);
 
             RuleFor(x => x.CarModelId).NotNull();
-            RuleFor(x => x.CarModelId).GreaterThan(0).When(x => x.NewCarModel == null);
-            RuleFor(x => x.NewCarModel).NotNull().When(x => x.CarModelId <= 0);
+            RuleFor(x => x.CarModelId).GreaterThan(0).When(x => x.CarModelName == null);
+            RuleFor(x => x.CarModelName).NotNull().When(x => x.CarModelId <= 0);
 
             RuleFor(x => x.TrimId).NotNull();
-            RuleFor(x => x.TrimId).GreaterThan(0).When(x => x.NewTrim == null);
-            RuleFor(x => x.NewTrim).NotNull().When(x => x.TrimId <= 0);
+            RuleFor(x => x.TrimId).GreaterThan(0).When(x => x.TrimName == null);
+            RuleFor(x => x.TrimName).NotNull().When(x => x.TrimId <= 0);
 
             var currentYear = DateTime.Now.Year;
             RuleFor(x => x.Year).GreaterThanOrEqualTo(currentYear - 20);
             RuleFor(x => x.Year).LessThanOrEqualTo(currentYear + 1);
 
             RuleFor(x => x.BodyTypeId).NotNull();
-            RuleFor(x => x.BodyTypeId).GreaterThan(0).When(x => x.NewBodyType == null);
-            RuleFor(x => x.NewBodyType).NotNull().When(x => x.BodyTypeId <= 0);
+            RuleFor(x => x.BodyTypeId).GreaterThan(0).When(x => x.BodyTypeName == null);
+            RuleFor(x => x.BodyTypeName).NotNull().When(x => x.BodyTypeId <= 0);
 
             RuleFor(x => x.Kms).GreaterThan(0);
             RuleFor(x => x.Kms).LessThanOrEqualTo(500000);
@@ -49,21 +49,21 @@ namespace CarHub.Domain.Validators
 
 
             RuleFor(x => x.ColorId).NotNull();
-            RuleFor(x => x.ColorId).GreaterThan(0).When(x => x.NewColor == null);
-            RuleFor(x => x.NewColor).NotNull().When(x => x.ColorId <= 0);
+            RuleFor(x => x.ColorId).GreaterThan(0).When(x => x.ColorName == null);
+            RuleFor(x => x.ColorName).NotNull().When(x => x.ColorId <= 0);
 
             RuleFor(x => x.NoOfSeats).InclusiveBetween(1, 10);
             RuleFor(x => x.NoOfDoors).InclusiveBetween(1, 10);
             RuleFor(x => x.NoOfCylinders).InclusiveBetween(1, 10);
 
             RuleFor(x => x.DriveTypeId).NotNull();
-            RuleFor(x => x.DriveTypeId).GreaterThan(0).When(x => x.NewDriveType == null);
-            RuleFor(x => x.NewDriveType).NotNull().When(x => x.DriveTypeId <= 0);
+            RuleFor(x => x.DriveTypeId).GreaterThan(0).When(x => x.DriveTypeName == null);
+            RuleFor(x => x.DriveTypeName).NotNull().When(x => x.DriveTypeId <= 0);
 
 
             RuleFor(x => x.FuelTypeId).NotNull();
-            RuleFor(x => x.FuelTypeId).GreaterThan(0).When(x => x.NewFuelType == null);
-            RuleFor(x => x.NewFuelType).NotNull().When(x => x.FuelTypeId <= 0);
+            RuleFor(x => x.FuelTypeId).GreaterThan(0).When(x => x.FuelTypeName == null);
+            RuleFor(x => x.FuelTypeName).NotNull().When(x => x.FuelTypeId <= 0);
 
             RuleFor(x => x.PurchaseDate).NotNull();
             RuleFor(x => Convert.ToDateTime(x.PurchaseDate)).NotNull();
@@ -73,8 +73,8 @@ namespace CarHub.Domain.Validators
             RuleFor(x => x.PurchasePrice).GreaterThan(0);
 
             RuleFor(x => x.PurchaseTypeId).NotNull();
-            RuleFor(x => x.PurchaseTypeId).GreaterThan(0).When(x => x.NewPurchaseType == null);
-            RuleFor(x => x.NewPurchaseType).NotNull().When(x => x.PurchaseTypeId <= 0);
+            RuleFor(x => x.PurchaseTypeId).GreaterThan(0).When(x => x.PurchaseTypeName == null);
+            RuleFor(x => x.PurchaseTypeName).NotNull().When(x => x.PurchaseTypeId <= 0);
 
 
             RuleFor(x => x.IsFeatured).NotNull();
