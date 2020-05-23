1122657e6556d2e6b7c2ecc61d592d4cd8c5c941
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-23 15:14:44 +1000

    Added valid redirection for log out

--- CarHub/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ CarHub/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CarHub.Areas.Identity.Pages.Account
 {
@@ -22,21 +22,16 @@ namespace CarHub.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public void OnGet()
-        {
-        }
-
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
+            if(returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
-            }
-            else
+            } else
             {
-                return RedirectToPage();
+                return RedirectToAction("Index", "Home");
             }
         }
     }
