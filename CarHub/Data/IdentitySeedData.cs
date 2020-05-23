2dff29076b06e5c1debaacd879de370997fd2c96
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-23 15:07:41 +1000

    Added a class for seeding Identity admin user

---
+++ CarHub/Data/IdentitySeedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarHub.Data
+{
+    public static class IdentitySeedData
+    {
+        private const string AdminUser = "Admin@carhub.com";
+        private const string AdminPassword = "P@ssword123";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            using(var scope = app.ApplicationServices.CreateScope())
+            {
+                var userManager = (UserManager<IdentityUser>)scope.ServiceProvider
+                    .GetService(typeof(UserManager<IdentityUser>));
+
+                IdentityUser user = await userManager.FindByIdAsync(AdminUser);
+
+                if(user == null)
+                {
+                    user = new IdentityUser(AdminUser);
+                    await userManager.CreateAsync(user, AdminPassword);
+                }
+            }
+        }
+    }
+}
