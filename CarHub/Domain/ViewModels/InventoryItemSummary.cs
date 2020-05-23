4c08c8db016931f093a288c5455dd9d0fc80e2f5
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-23 19:00:03 +1000

    Implemented link to view details page from recent car widget

---
+++ CarHub/Domain/ViewModels/InventoryItemSummary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarHub.Domain.ViewModels
+{
+    public class InventoryItemSummary
+    {
+        public string Image { get; set; }
+
+        public string Price { get; set; }
+
+        public string HeadLine { get; set; }
+
+        public string InventoryId { get; set; }
+    }
+}
