e8d79ed6fae5813fdd98c04a061f0f7b7e58f9a3
Dilip Agheda <dilip_agheda@yahoo.com>
2020-05-29 21:21:59 +1000

    Added culture as AU so that it works consistently on Azure

--- CarHub/Startup.cs
+++ CarHub/Startup.cs
@@ -69,6 +69,19 @@ namespace CarHub
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var enAUCulture = new CultureInfo("en-AU");
+            var localizationOptions = new RequestLocalizationOptions()
+            {
+                SupportedCultures = new List<CultureInfo>() { enAUCulture },
+                DefaultRequestCulture = new RequestCulture(enAUCulture),
+                FallBackToParentCultures = false,
+                FallBackToParentUICultures = false,
+                RequestCultureProviders = null
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
